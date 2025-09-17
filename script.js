ff3ecc6b731a47ea521f42144ce0e6a5fc3075f4
Vuir <Taevio@hotmail.com>
2025-09-17 12:14:10 +0300

    Done TODO-7. Make a Regex to find the words that contain specefic pattern.

--- script.js
+++ script.js
@@ -135,5 +135,12 @@ Given: const words = ["ban", "babble", "make", "flab"];
 3) For matches, log "<word> matches!".
 4) Display the words that matches the pattern.
 */
-
+const words = ["ban", "babble", "make", "flab"];
+const ab= /ab/;
+words.forEach(function(word) {
+   if (ab.test(word)) {
+      console.log(word + " matches!");
+   }
+}
+);
 // End of Advance JavaScript Lab — good luck!
